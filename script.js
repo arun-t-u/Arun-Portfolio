e63a938859789b453b484e73ba852a7b3ce846f1
Arun TU <arun.tu@dc.cognicor.com>
2024-11-19 14:59:09 +0530

    header responsive

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-    
+// element toggle function
+const elemToggleFunc = function (elem) { elem.classList.toggle("active"); }
+
+
+// Navbar Toggle
+const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
+const navbar = document.querySelector("[data-navbar]");
+
+navToggleBtn.addEventListener("click", function () {
+    elemToggleFunc(navToggleBtn);
+    elemToggleFunc(navbar);
+    elemToggleFunc(document.body);
+});
